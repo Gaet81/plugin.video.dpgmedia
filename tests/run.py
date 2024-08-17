3427f1d02b65b0b5023a59a24d1b244a924e548e
Gaet81 <22379937+Gaet81@users.noreply.github.com>
2024-08-17 10:07:34 +0000

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -16,12 +16,12 @@ from resources.lib import addon  # noqa: E402  pylint: disable=wrong-import-posi
 
 if len(sys.argv) <= 1:
     print(
-        "%s: URI argument missing\nTry '%s plugin://plugin.video.vtm.go/' to test." % (sys.argv[0], sys.argv[0]))
+        "%s: URI argument missing\nTry '%s plugin://plugin.video.dpgmedia/' to test." % (sys.argv[0], sys.argv[0]))
     sys.exit(1)
 
 # Also support bare paths like /recent/2
 if not sys.argv[1].startswith('plugin://'):
-    sys.argv[1] = 'plugin://plugin.video.vtm.go' + sys.argv[1]
+    sys.argv[1] = 'plugin://plugin.video.dpgmedia' + sys.argv[1]
 
 # Split path and args
 try:
