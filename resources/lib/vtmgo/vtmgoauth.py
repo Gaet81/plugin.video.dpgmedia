d0f940a323b2a2475ba5e5f6b47c70f4f0480cce
Gaet81 <22379937+Gaet81@users.noreply.github.com>
2024-08-17 14:16:05 +0000

    Update vtmgoauth.py

--- resources/lib/vtmgo/vtmgoauth.py
+++ resources/lib/vtmgo/vtmgoauth.py
@@ -145,7 +145,7 @@ class VtmGoAuth:
         
         resp2 = util.http_post(URL_COMPTE_LOGIN, form=payload, headers=headers)
         xbmc.log(resp2.text,xbmc.LOGINFO)
-        xbmc.log(resp2.headers,xbmc.LOGINFO)
+        xbmc.log(str(resp2.headers),xbmc.LOGINFO)
         xbmc.log(resp2.cookies,xbmc.LOGINFO)
         auth_info = json.loads(resp2.text)
         if "UID" not in auth_info:
